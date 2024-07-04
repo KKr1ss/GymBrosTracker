ef6dca65b200e57f415749de600bac2ec4d661cf
Kiss Krisztofer Béla <kisskb@mav.hu>
2024-07-04 12:25:02 +0200

    MAUI XAML migrated to MAUI Blazor Hypbrid

--- src/GymBrosTracker.UI/MainPage.xaml.cs
+++ src/GymBrosTracker.UI/MainPage.xaml.cs
@@ -1,16 +1,10 @@
-﻿using GymBrosTracker.UI.ViewModels;
-
-namespace GymBrosTracker.UI
+﻿namespace GymBrosTracker.UI
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage(MainViewModel viewModel)
+        public MainPage()
         {
             InitializeComponent();
-            BindingContext = viewModel;
         }
-
-        
     }
-
 }
--- src/GymBrosTracker.UI/MauiProgram.cs
+++ src/GymBrosTracker.UI/MauiProgram.cs
@@ -1,8 +1,6 @@
 ﻿using GymBrosTracker.Domain;
-using GymBrosTracker.UI.ViewModels;
 using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Core;
 using Serilog.Exceptions;
 
 namespace GymBrosTracker.UI
@@ -17,26 +15,27 @@ namespace GymBrosTracker.UI
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddMauiBlazorWebView();
+
             builder.Logging.ClearProviders();
-            //Log.Logger = new LoggerConfiguration()
-            //    .Enrich.WithExceptionDetails()
-            //    .Enrich.FromLogContext()
-            //    .WriteTo.SQLite(Domain.Helpers.Constants.DBPath)
-            //    .CreateLogger();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithExceptionDetails()
+                .Enrich.FromLogContext()
+                .WriteTo.SQLite(Domain.Helpers.Constants.DBPath)
+                .CreateLogger();
             builder.Services.AddLogging(logging =>
             {
                 logging.AddSerilog(dispose: true);
             });
 
             builder.RegisterServices();
-            builder.RegisterViewModels();
-            builder.RegisterViews();
-//#if DEBUG
-//            builder.Logging.AddDebug();
-//#endif
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
             Log.Logger.Information("Application has been started!");
             return builder.Build();
         }
@@ -47,19 +46,5 @@ namespace GymBrosTracker.UI
 
             return builder;
         }
-
-        public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder builder)
-        {
-            builder.Services.AddSingleton<MainViewModel>();
-            return builder;
-        }
-
-        public static MauiAppBuilder RegisterViews(this MauiAppBuilder builder)
-        {
-            builder.Services.AddSingleton<MainPage>();
-
-            return builder;
-        }
-
     }
 }
